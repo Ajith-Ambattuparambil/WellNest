8415449ddc8fe0e022ec77f2c3cfa7995eb9eeea
Ajith Mani <ajithambattuparambil@gmail.com>
2025-03-29 15:19:22 +0530

    Initialize NotificationService and implement AuthWrapper for user authentication navigation

--- Project/family_member/lib/main.dart
+++ Project/family_member/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:family_member/screens/landingpage.dart';
+import 'package:family_member/screens/manage_profile.dart';
+import 'package:family_member/services/notification_services.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -8,7 +10,7 @@ Future<void> main() async {
     anonKey:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhpZ2lkeHR1Z2VtcWpxeHh1cGJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkxODc0NTksImV4cCI6MjA1NDc2MzQ1OX0._nRKz4mIaREHLmv5mihabn9kwI180MBZIxwHbqkklQ8',
   );
-
+  await NotificationService.init();
   runApp(MainApp());
 }
 
@@ -19,6 +21,23 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: Landingpage());
+    return const MaterialApp(home: AuthWrapper());
+  }
+}
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Check if the user is logged in
+    final session = supabase.auth.currentSession;
+
+    // Navigate to the appropriate screen based on the authentication state
+    if (session != null) {
+      return ManageProfile(); // Replace with your home screen widget
+    } else {
+      return Landingpage(); // Replace with your auth page widget
+    }
   }
 }
