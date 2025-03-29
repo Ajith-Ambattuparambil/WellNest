f532507764003a3421a42a3e30725a6f054260b6
Ajith Mani <ajithambattuparambil@gmail.com>
2025-03-29 15:18:10 +0530

    Remove immediate notification display during app initialization

--- Project/caretaker_wellnest/lib/main.dart
+++ Project/caretaker_wellnest/lib/main.dart
@@ -13,7 +13,6 @@ Future<void> main() async {
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhpZ2lkeHR1Z2VtcWpxeHh1cGJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkxODc0NTksImV4cCI6MjA1NDc2MzQ1OX0._nRKz4mIaREHLmv5mihabn9kwI180MBZIxwHbqkklQ8',
   );
   await RoutineNotificationService.init();
-  await RoutineNotificationService.showImmediateNotification();
   runApp(MainApp());
 }
 
